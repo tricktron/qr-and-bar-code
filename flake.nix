ccd6f793fcad8de97ee3d6180901c86434526b59
Thibault Gagnaux <tgagnaux@gmail.com>
2021-09-15 19:36:29 +0200

    chore: init fitbit project setup

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "qr-and-bar-code-dev-shell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs-14_x
+            purescript
+          ];
+        };
+      });
+}
