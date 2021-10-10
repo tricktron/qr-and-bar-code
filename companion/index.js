5f424b583f68912c65df4185474250aade1da305
Thibault Gagnaux <tgagnaux@gmail.com>
2021-10-10 11:11:28 +0200

    refactor: adds new purescript library code

--- companion/index.js
+++ companion/index.js
@@ -2,9 +2,7 @@ import { outbox } from "file-transfer";
 import { Image } from "image";
 import { device } from "peer";
 import { settingsStorage } from "settings";
-import * as PS from "./qr-reader-lib-es6.js";
-import { encode, TXIOutputFormat } from "@fitbit/image-codec-txi";
-import qrCodeGenerator from "qrcode-generator";
+import { scanQrCode, createBase64PNGQrCode  } from "../node_modules/qr-reader";
 
 settingsStorage.setItem("screenWidth", device.screen.width);
 settingsStorage.setItem("screenHeight", device.screen.height);
@@ -15,36 +13,23 @@ settingsStorage.onchange = function (evt) {
     }
 };
 
+async function decodeQrCode(imageUri) {
+    const image = await Image.from(imageUri);
+    image.drawingContext.drawImage(image.image, 0, 0);
+    const rgba = await image.drawingContext.getImageData(0, 0, image.width, image.height);
+    const decodedText = scanQrCode(rgba.data)(image.width)(image.height).value0.value0;
+    const base64QrCode = createBase64PNGQrCode(decodedText)(image.width);
+    return await Image.from(base64QrCode);
+}
+
 const compressAndTransferImage = async settingsValue => {
     const imageData = JSON.parse(settingsValue);
     try {
-        const image = await Image.from(imageData.imageUri);
-        image.drawingContext.drawImage(image.image, 0, 0);
-        const rgba = await image.drawingContext.getImageData(0, 0, image.width, image.height);
-        const qrCode = PS.__reserved_default.scanQrCode(rgba.data)(image.width)(image.height);
-        console.log(qrCode);
-        const encodedText = qrCode.value0.value0.text;
-        const qr = qrCodeGenerator(0, 'M');
-        qr.addData(encodedText);
-        qr.make();
-        const moduleSize = qr.getModuleCount();
-        const scaledModuleSize = 300 / (moduleSize + 8);
-        const margin = 2 * scaledModuleSize;
-        console.log("The module size is:", moduleSize);
-        console.log("The scaled module size is:", scaledModuleSize);
-        console.log("The margin is:", margin);
-        //const base64QrImage = qr.createDataURL();
-        const base64QrImage = qr.createDataURL(scaledModuleSize, 4 * scaledModuleSize);
-        console.log(base64QrImage);
-        const pngBase64Image = base64QrImage.replace("data:image/gif", "data:image/png");
-        const qrImage = await Image.from(pngBase64Image);
-        console.log(qrImage.width);
-        //const buf = encode(qrCodeImage, { outputFormat: TXIOutputFormat.RGB565, rle: true });
+        const qrImage = await decodeQrCode(imageData.imageUri);
         const buf = await qrImage.export("image/vnd.fitbit.txi");
         const file = await outbox.enqueue(`${Date.now()}.txi`, buf);
         console.log(`Enqueued ${file.name}`);
     } catch (error) {
-        console.log(error);
         console.log(error.message);
     }
 }
